bfb02363dd58b71a5e87d3d8d4c9a68dc19b0cfc
jawad <jawadusaid11@gmail.com>
2022-02-22 00:15:47 +0530

    Array

---
+++ Day10/src/day10/ArrayCreate.java
@@ -0,0 +1,23 @@
+package day10;
+
+public class ArrayCreate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[50];
+		a[0]=10;
+		a[1]=11;
+		a[2]=23;
+		a[3]=456;
+		a[4]=34;
+		a[5]=888;
+		
+		System.out.println("Array elements are: ");    
+		for (int i=0; i<a[i]; i++)   
+		{  
+		System.out.println(a[i]);  
+		}  
+	}
+
+}
+
